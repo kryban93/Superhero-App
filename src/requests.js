7b20763afc86b8f9bdba45f5d8b4cb20f6e5b275
kryban93 <kryban1993@gmail.com>
2020-10-22 10:29:11 +0200

    changes in axios fetching

--- src/requests.js
+++ src/requests.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const ACCES_TOKEN = 1533153713527712;
 
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export const getBasicHeroInfoById = async (id) => {
-  const { data: powerstats } = await axios.get(`https://superheroapi.com/api/${ACCES_TOKEN}/${id}/powerstats`);
-  const { data: image } = await axios.get(`https://superheroapi.com/api/${ACCES_TOKEN}/${id}/image`);
+  const { data: powerstats } = await axios.get(
+    `https://superheroapi.com/api/${ACCES_TOKEN}/${id}/powerstats`
+  );
+  const { data: image } = await axios.get(
+    `https://superheroapi.com/api/${ACCES_TOKEN}/${id}/image`
+  );
 
   return { name: powerstats.name, powerstats, imgUrl: image.url, id: powerstats.id };
 };
